a715da58ee9d5db8ec787796770328c6f18cdf82
FlutLab.io <>
2022-04-04 18:41:47 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BelajarAppBar(),
+      home: AppBarMusic(),
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class BelajarAppBar extends StatelessWidget {
+class AppBarMusic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
